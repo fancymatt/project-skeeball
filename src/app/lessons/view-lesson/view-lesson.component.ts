306bf50d8d6e45746efca90a29a8bce2eed126a4
Matt Henry <matt@innovativelanguage.com>
2018-06-05 19:25:42 -0600

    Adds some animations to view lesson

--- src/app/lessons/view-lesson/view-lesson.component.ts
+++ src/app/lessons/view-lesson/view-lesson.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import {Lesson} from '../lesson.model';
 import {LessonService} from '../lesson.service';
 import {Line} from '../../lines/line.model';
@@ -6,15 +7,31 @@ import {Line} from '../../lines/line.model';
 @Component({
   selector: 'app-view-lesson',
   templateUrl: './view-lesson.component.html',
-  styleUrls: ['./view-lesson.component.css']
+  styleUrls: ['./view-lesson.component.css'],
+  animations: [
+    trigger('transitionPresentTextState', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateY(0px)'
+      })),
+      state('out', style({
+        opacity: 0,
+        transform: 'translateY(-10px)'
+      })),
+      transition('in => out', animate('300ms ease-out')),
+      transition('out => in', animate('1000ms ease-out')),
+      transition('void => in', animate('1000ms ease-out'))
+    ])
+  ]
 })
 export class ViewLessonComponent implements OnInit {
   currentLesson: Lesson;
   currentLineIndex: number;
   totalLines: number;
   currentLine: Line;
-  options: {text: string, correct: boolean, selected: boolean}[] = [];
+  mcOptions: {text: string, correct: boolean, selected: boolean}[] = [];
   preventAdvance: boolean;
+  isTransitioningBetweenLines = 'in';
 
   constructor(private lessonService: LessonService) { }
 
@@ -26,39 +43,23 @@ export class ViewLessonComponent implements OnInit {
     this.handleMultipleChoice();
   }
 
-  advanceToNextLine() {
-    if (this.currentLineIndex < this.totalLines - 1) {
-      this.currentLineIndex++;
-      this.currentLine = this.currentLesson.lines[this.currentLineIndex];
-      this.handleMultipleChoice();
-    }
-  }
-
-  advanceToPreviousLine() {
-    if (this.currentLineIndex > 0) {
-      this.currentLineIndex--;
-      this.currentLine = this.currentLesson.lines[this.currentLineIndex];
-      this.handleMultipleChoice();
-    }
-  }
 
   handleMultipleChoice(): void {
     if (this.currentLine.type !== 'Multiple Choice') { return; }
 
-    this.options.push({text: this.currentLine.mcAnswerCorrect, correct: true, selected: false});
-    this.options.push({text: this.currentLine.mcAnswerIncorrect1, correct: false, selected: false});
-    this.options.push({text: this.currentLine.mcAnswerIncorrect2, correct: false, selected: false});
-    this.options.push({text: this.currentLine.mcAnswerIncorrect3, correct: false, selected: false});
+    this.mcOptions.push({text: this.currentLine.mcAnswerCorrect, correct: true, selected: false});
+    this.mcOptions.push({text: this.currentLine.mcAnswerIncorrect1, correct: false, selected: false});
+    this.mcOptions.push({text: this.currentLine.mcAnswerIncorrect2, correct: false, selected: false});
+    this.mcOptions.push({text: this.currentLine.mcAnswerIncorrect3, correct: false, selected: false});
 
-    this.shuffleArray(this.options);
+    this.shuffleArray(this.mcOptions);
     this.preventAdvance = true;
   }
 
   onClickOption(selectedOption: {text: string, correct: boolean, selected: boolean}) {
     if (selectedOption.correct) {
       this.preventAdvance = false;
-      this.options = [];
-      this.advanceToNextLine();
+      this.mcOptions = [];
     }
     selectedOption.selected = true;
   }
@@ -70,5 +71,33 @@ export class ViewLessonComponent implements OnInit {
     }
   }
 
+  transitionToLine(nextLineIndex: number) {
+    if (nextLineIndex > 0 && nextLineIndex < this.totalLines) {
+      this.isTransitioningBetweenLines = 'out';
+      setTimeout(() => {
+        this.currentLineIndex = nextLineIndex;
+        this.currentLine = this.currentLesson.lines[nextLineIndex];
+        this.handleMultipleChoice();
+        this.isTransitioningBetweenLines = 'in';
+      }, 1000);
+    }
+  }
 
 }
+
+/*
+
+How to handle animation state.
+Make a component for every line type (will have to do this eventually)
+Each line type will respond to transition-in, transition-out events in its own way.
+Even when transitioning between two line types of the same type, we remove and initialize a new type.
+Can't be fancy with keeping a static video track if we do that.
+Each line type's CSS can be completely independent from the others.
+
+view-lesson - lesson-controls
+  view-explanation
+  view-example
+  view-mcQuestion
+  view-title
+
+ */
