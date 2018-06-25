2ee7139352ba4bbcdd96bd083872c79db4ab1e9a
Matt Henry <matt@innovativelanguage.com>
2018-06-25 13:10:53 -0600

    Add CRUD methods for objectives

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { catchError } from 'rxjs/operators';
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
 
 import { Lesson } from '../lessons/lesson.model';
-import {SkeeballError} from './skeeballError';
-import {Vocab} from '../vocab/vocab.model';
-import {IdGenService} from './id-gen.service';
+import { SkeeballError } from './skeeballError';
+import { Vocab } from '../vocab/vocab.model';
+import { IdGenService } from './id-gen.service';
+import { Objective } from '../objectives/objective.model';
 
 @Injectable()
 export class DataService {
   private apiEndpoint = 'https://9ygt6xpwi7.execute-api.us-west-1.amazonaws.com/dev/';
 
-  constructor(private http: HttpClient, private uuidService: IdGenService) { }
+  constructor(private http: HttpClient, private uuidService: IdGenService) {
+  }
 
   // Lessons
 
@@ -46,7 +47,7 @@ export class DataService {
   // Vocabs
 
   getAllVocabs(): Observable<Vocab[]> {
-    const url = this.apiEndpoint + 'vocabs/';
+    const url = this.apiEndpoint + 'vocabs';
     return this.http.get<Vocab[]>(url);
   }
 
@@ -63,7 +64,6 @@ export class DataService {
 
   updateVocab(updatedVocab: Vocab): Observable<void> {
     const url = this.apiEndpoint + 'vocabs/' + updatedVocab.id;
-    console.log(updatedVocab);
     return this.http.put<void>(url, updatedVocab);
   }
 
@@ -79,6 +79,34 @@ export class DataService {
     dataError.message = error.statusText;
     dataError.friendlyMessage = 'An error occurred retrieving data.';
     return ErrorObservable.create(dataError);
+  }
+
+  // Objectives
+
+  getAllObjectives(): Observable<Objective[]> {
+    const url = this.apiEndpoint + 'objectives';
+    return this.http.get<Objective[]>(url);
+  }
+
+  getObjective(id: string): Observable<Objective> {
+    const url = this.apiEndpoint + 'objectives';
+    return this.http.get<Objective>(url);
+  }
+
+  createObjective(newObjective: Objective): Observable<Objective> {
+    const url = this.apiEndpoint + 'objectives';
+    newObjective.id = this.uuidService.generateUniqueId();
+    return this.http.post<Objective>(url, newObjective);
+  }
+
+  updateObjective(updatedObjective: Objective): Observable<void> {
+    const url = this.apiEndpoint + 'objectives/' + updatedObjective.id;
+    return this.http.put<void>(url, updatedObjective);
+  }
+
+  deleteObjective(deletedObjective: Objective): Observable<void> {
+    const url = this.apiEndpoint + 'objectives/' + deletedObjective.id;
+    return this.http.delete<void>(url);
   }
 
 }
